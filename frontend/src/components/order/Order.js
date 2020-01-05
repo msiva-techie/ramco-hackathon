f96afb00a2722d96e62e8aac164d551fb2c77ffe
Siva M <siva.m@ramco.com>
2020-01-05 12:51:10 +0530

    working commit

--- frontend/src/components/order/Order.js
+++ frontend/src/components/order/Order.js
@@ -12,7 +12,7 @@ export class Order extends Component {
     localStorage.removeItem("productName");
     this.state = {
       count: 0,
-      result: this.output
+      result: ""
     };
   }
   componentDidMount() {
@@ -27,6 +27,10 @@ export class Order extends Component {
         longitude: this.props.coords.longitude,
         productName: this.productName,
         count: this.state.count
+      });
+      this.setState({
+        ...this.state,
+        result: this.output
       });
       console.log("output....", this.output);
     } else {
@@ -45,72 +49,90 @@ export class Order extends Component {
   };
   render() {
     return (
-      <div style={{ float: "left" }}>
-        <div className="card1" style={{ marginLeft: "80px" }}>
-          (
-          <img
-            src={require("./../../result.png")}
-            style={{
-              width: "100%",
-              height: "350px",
-              marginTop: "0"
-            }}
-            className="img1"
-            alt="picture"
-          />
-          )<span style={{ fontSize: "18px", fontWeight: "bold" }}></span>
-        </div>
-        <div className="submit" style={{ marginLeft: "80px" }}>
-          <span style={{ fontSize: "18px", fontWeight: "bold" }}>
-            <div style={{ marginTop: "100px", marginLeft: "20px" }}>
-              <button
-                className="btn btn-outline-primary"
-                onClick={() => {
-                  this.decrement();
-                }}
-              >
-                Decrement
-              </button>
-              <span style={{ marginLeft: "80px" }}>
-                {this.state.count} quantity
-              </span>
+      <div className="container">
+        <div style={{ float: "left" }}>
+          <div className="card1" style={{ marginLeft: "80px" }}>
+            (
+            <img
+              src={require("./../../result.png")}
+              style={{
+                width: "100%",
+                height: "350px",
+                marginTop: "0"
+              }}
+              className="img1"
+              alt="picture"
+            />
+            )<span style={{ fontSize: "18px", fontWeight: "bold" }}></span>
+          </div>
+          <div className="submit" style={{ marginLeft: "80px" }}>
+            <span style={{ fontSize: "18px", fontWeight: "bold" }}>
+              <div style={{ marginTop: "100px", marginLeft: "20px" }}>
+                <button
+                  className="btn btn-outline-primary"
+                  onClick={() => {
+                    this.decrement();
+                  }}
+                >
+                  Decrement
+                </button>
+                <span style={{ marginLeft: "80px" }}>
+                  {this.state.count} quantity
+                </span>
 
-              <button
-                className="btn btn-outline-primary"
-                style={{ marginLeft: "100px" }}
-                onClick={() => {
-                  this.increment();
-                }}
-              >
-                Increment
-              </button>
-              <button
-                className="btn btn-outline-primary"
-                style={{ marginLeft: "40px", marginTop: "50px", width: "80%" }}
-                onClick={() => {
-                  this.getETA();
-                }}
-              >
-                order
-              </button>
+                <button
+                  className="btn btn-outline-primary"
+                  style={{ marginLeft: "100px" }}
+                  onClick={() => {
+                    this.increment();
+                  }}
+                >
+                  Increment
+                </button>
+                <button
+                  className="btn btn-outline-primary"
+                  style={{
+                    marginLeft: "40px",
+                    marginTop: "50px",
+                    width: "80%"
+                  }}
+                  onClick={() => {
+                    this.getETA();
+                  }}
+                >
+                  order
+                </button>
+              </div>
+            </span>
+          </div>
+          <div></div>
+          {this.state.result ? (
+            <div className="card output">
+              <div className="card-body">
+                <h5 className="card-title">Order Details</h5>
+                <p className="card-text">
+                  {this.state.result ? (
+                    <ul>
+                      <li>ETA: {this.state.result.ETA}</li>
+                      <li>Distance:{this.state.result.distance}</li>
+                      <li>
+                        Name of the Delivery person:
+                        {this.state.result.result.deliveryBoy.name}
+                      </li>
+                      <li>
+                        Location of the Shop: {this.state.result.result.shop}
+                      </li>
+                      <li>
+                        Location of the user:{this.props.coords.latitude}{" "}
+                        {this.props.coords.longitude}
+                      </li>
+                    </ul>
+                  ) : (
+                    ""
+                  )}
+                </p>
+              </div>
             </div>
-          </span>
-        </div>
-        <div>
-          {this.output ? (
-            <ul>
-              <li>ETA: {this.output.ETA}</li>
-              <li>Distance:{this.output.distance}</li>
-              <li>
-                Name of the Delivery person:
-                {this.output.result.deliveryBoy.name}
-              </li>
-              <li>Location of the Shop: {this.output.result.shop}</li>
-              <li>
-                Location of the user:{this.props.coords.latitude}{" "}
-                {this.props.coords.longitude}
-              </li>
-            </ul>
           ) : (
             ""
           )}
